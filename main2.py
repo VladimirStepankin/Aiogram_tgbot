67dfba77233bdf4f56a2275772cb3c0ce0b0bb67
Vladimir Stepankin <vladimirstepankin@mail.ru>
2023-03-06 12:37:42 +0300

    added main2.py

--- main2.py
+++ main2.py
@@ -1,7 +1,12 @@
 # Inline клавиатура
 from aiogram import Bot, executor, Dispatcher, types
+from aiogram.dispatcher.filters import Text
+from aiogram.types import InputFile, Message
 from keybords import kb, ikb
-
+import random
+from glob import glob
+import os
+from PIL import Image
 
 from Config import TOKEN_API
 
@@ -9,24 +14,54 @@ from Config import TOKEN_API
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
+HELP_COMMAND = """
+/start
+/help
+/description
+/give
+"""
+
+arr_images = glob('images/*')
+
+
 
 async def on_startup(_):  # сообщеение в терминале о запуске бота
     print('Бот был успешно запущен')
 
 
 @dp.message_handler(commands=['start'])
-async def links_command(message: types.message):
+async def links_command(message: types.Message):
     await message.answer(text="Добро пожаловать в главное меню",
                          reply_markup=kb)
     await message.delete()
 
 
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await bot.send_message(chat_id=message.chat.id,
+                           text=HELP_COMMAND)
+    await message.delete()
+
+
 @dp.message_handler(commands=['vote'])
-async def vote_command(message: types.message):
+async def vote_command(message: types.Message):
+    photo = open(random.choice(arr_images), 'rb')
     await bot.send_photo(chat_id=message.chat.id,
-                         photo="https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg",
+                         photo=photo,
                          caption='Как фото?',
                          reply_markup=ikb)
+    await message.delete()
+
+
+@dp.message_handler(Text(equals='Рандомное фото'))
+async def rendom_photo_command(message: types.Message):
+    photo = open(random.choice(arr_images), 'rb')
+    await bot.send_photo(chat_id=message.chat.id,
+                         photo=photo,
+                         caption='Вот ваше фото!!!',
+                         reply_markup=kb)
+    
+    await message.delete()
 
 
 @dp.callback_query_handler()
