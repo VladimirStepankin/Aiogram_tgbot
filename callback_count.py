d2f89219523070d8242d3dd3405ce2454ca9337a
Vladimir Stepankin <vladimirstepankin@mail.ru>
2023-03-11 15:34:14 +0300

    add callback_count.py

---
+++ callback_count.py
@@ -0,0 +1,33 @@
+"The callback data count"
+
+from aiogram import Dispatcher, executor, Bot, types
+from keybords import get_inline_keybord
+from Config import TOKEN_API
+
+bot = Bot(TOKEN_API)
+db = Dispatcher(bot)
+number = 0
+
+
+@db.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer(f'The current number is {number}',
+                         reply_markup=get_inline_keybord())
+
+
+@db.callback_query_handler(lambda callback_query: callback_query.data.startswith('btn'))
+async def ikb_cb_handler(callback: types.CallbackQuery):
+    global number
+    if callback.data == 'btn_increase':
+        number += 1
+        await callback.message.edit_text(f'The current number is {number}',
+                                         reply_markup=get_inline_keybord())
+    elif callback.data == 'btn_decrease':
+        number -= 1
+        await callback.message.edit_text(f'The current number is {number}',
+                                         reply_markup=get_inline_keybord())
+    else:
+        1/0
+
+if __name__ == "__main__":
+    executor.start_polling(dispatcher=db, skip_updates=True)
